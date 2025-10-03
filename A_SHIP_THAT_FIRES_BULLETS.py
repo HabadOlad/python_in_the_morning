1574e7ee66d962fa3b19648f1be283f92305d845
Habad Olad <olad.hab@gmail.com>
2025-10-03 21:47:11 +0100

    feat:beginning a new game

--- A_SHIP_THAT_FIRES_BULLETS.py
+++ A_SHIP_THAT_FIRES_BULLETS.py
@@ -1,5 +1,9 @@
 #Python in the morning Friday the third of October 2025
 #Habad Olad 03/10/20
+
+print('-'* 50)
 username = input(str("what is your name weary warrior?"))
 
 print(f"Hello, {username} welcome to the coolest game ever! A ship that fires bullets!")
+
+
