be5824883b2070933e20accca8fa69d44eada905
habadOlad <olad.hab@gmail.com>
2025-10-03 21:45:50 +0100

    Add files via upload

---
+++ A_SHIP_THAT_FIRES_BULLETS.py
@@ -0,0 +1,5 @@
+#Python in the morning Friday the third of October 2025
+#Habad Olad 03/10/20
+username = input(str("what is your name weary warrior?"))
+
+print(f"Hello, {username} welcome to the coolest game ever! A ship that fires bullets!")
