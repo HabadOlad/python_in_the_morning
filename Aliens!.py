46724442b87d90ef2608216732f161b3c05a9f2a
Habad Olad <olad.hab@gmail.com>
2025-10-03 21:53:42 +0100

    feat:aliens! new game

---
+++ Aliens!.py
@@ -0,0 +1,5 @@
+#Aliens!
+print("---"*100)
+username = input(str("What is your name hero of earth?"))
+aliens = input(str("What are the aliens called?"))
+print(f"Welcome! {username} thank you for aiding us in this battle against the {aliens}!")
