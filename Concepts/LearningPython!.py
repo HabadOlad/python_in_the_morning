d6b0c247a8ab7baa19280f3e258fc2057c420d54
Habad Olad <olad.hab@gmail.com>
2025-12-23 06:59:02 +0000

    feat:refresher for my brain

---
+++ Concepts/LearningPython!.py
@@ -0,0 +1,11 @@
+#this is a comment in python
+
+'''
+This is a multi line comment in python ,
+
+although technically they are string literals,
+
+but can be used as comments if they are not assigned to a variable
+
+'''
+
