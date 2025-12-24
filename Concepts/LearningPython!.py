c7327222229436fff6287a02f8335371c01ec562
Habad Olad <olad.hab@gmail.com>
2025-12-24 08:33:14 +0000

    feat:learning

--- Concepts/LearningPython!.py
+++ Concepts/LearningPython!.py
@@ -9,3 +9,35 @@ but can be used as comments if they are not assigned to a variable
 
 '''
 
+'''
+In fact, it’s now fairly safe to say that Python
+ played a pivotal role in changing the world. 
+ By spearheading a shift from statically typed compiled languages to dynamically typed scripting languages, 
+ Python ushered in changes that were at least as profound as those of the earlier transition from 
+ machine language to compiled languages. 
+
+ 
+'''
+
+'''
+Compiled Languages: In a compiled language, the entire
+source code is translated into machine code by a
+program called a compiler before the program is 
+run. This results in a standalone executable file
+(like an .exe on Windows).Workflow: Code $\rightarrow$ Compiler $\rightarrow$ Executable File $\rightarrow$ Output.
+Performance: Generally much faster because the translation is done once, and the computer runs the pre-optimized machine code directly.
+Error Detection: Errors are caught during the "build" phase. 
+If there is a syntax error, the program won't even finish compiling.Examples: C, C++, Rust, Go, Swift.
+'''
+
+'''
+Interpreted Languages
+In an interpreted language, 
+the source code is not translated all at once. Instead, 
+a program called an interpreter reads and executes the code 
+line-by-line, on the fly, as the program is running.Workflow: Code $\rightarrow$ Interpreter $\rightarrow$ Output.
+Performance: Typically slower because the computer has to translate each line of code every time it runs.
+Error Detection: Errors are caught at "runtime." The program will start running and only crash when it hits the specific 
+line containing the error.
+Examples: Python, JavaScript, Ruby, PHP.
+'''
