423cbe363850bab5155d2675d2c39c9ef1de2f8f
Habad Olad <olad.hab@gmail.com>
2026-02-27 11:42:39 +0000

    feat:learning

--- Concepts/loops.py
+++ Concepts/loops.py
@@ -9,14 +9,34 @@ Starting on the next line, an indented block of code (called the while clause or
 
 '''
 
-spam = 0
-if spam < 5:
-    print('Hello, world.')
-    spam = spam + 1
+# spam = 0
+# if spam < 5:
+#     print('Hello, world.')
+#     spam = spam + 1
 
 #compare this to this which uses a while statement
 
-spam = 0
-while spam < 5:
-    print('Hello, world.')
-    spam = spam + 1
+# spam = 0
+# while spam < 5:
+#     print('Hello, world.')
+#     spam = spam + 1
+
+'''
+The code with the if statement checks the condition, and it prints "Hello, world." 
+only once if that condition is true. The code with the while loop, on the other hand,
+ will print it five times. The loop stops after five prints because the integer in spam 
+ increases by one at the end of each loop iteration, which means that the loop will
+execute five times before spam < 5 is False.
+
+
+
+'''
+
+#break statements
+
+while True:
+    print('Please type your name.')
+    name = input('>')
+    if name == 'your name':
+        break
+print('Thank you!')
