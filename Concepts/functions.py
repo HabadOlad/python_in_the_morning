109341dbc7bd423a9fd437e6615112e6af558cb7
Habad Olad <olad.hab@gmail.com>
2026-03-01 07:00:06 +0000

    feat:learning

---
+++ Concepts/functions.py
@@ -0,0 +1,9 @@
+'''
+A function is like a mini program within a program. 
+Python provides several built-in functions, such as the print(), 
+input(), and len() functions from the previous chapters,
+but you can also write your own. In this chapter, you’ll create functions,
+ explore the call stack used to determine the order in which functions in a 
+ program run, and learn about the scope of variables inside and outside functions.
+'''
+
